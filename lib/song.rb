794eeab386bdbc6da0a4dfd62e82552f66033d74
Kayla Hansen <shiverr@gmail.com>
2017-10-21 20:46:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,8 @@ class Song
   end
 
   def self.new_by_filename(file_name)
-    new_instance= Song.new(file_name)
-    new_instance.title = Song.new(file_name).split(" - ")[1]
+    new_instance = Song.new(file_name)
+    new_instance.title = file_name.split(" - ")[1]
     new_instance
   end
 
