dc8539bb169a0e3436d037a7ea4243d79c3ecd03
Kayla Hansen <shiverr@gmail.com>
2017-10-21 21:10:36 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,12 +2,12 @@ class MP3Importer
   attr_accessor :path
 
   def initialize(test_music_path)
-    test_music_path = "./spec/fixtures/mp3s"
     @path = path
   end
 
   def files
-    @files = Dir["./db/mp3s/*"]
+    @files = Dir.foreach("./db/mp3s/*") do |item|
+      
   end
 
   def import(list_of_file_names)
