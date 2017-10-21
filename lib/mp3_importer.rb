e2fc4770afe4ca4e927a2611642abe7e0f871fdd
Kayla Hansen <shiverr@gmail.com>
2017-10-21 21:47:37 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -14,7 +14,6 @@ class MP3Importer
  end
 
   def import(list_of_file_names)
-    list_of_file_names = Dir.entries(@path)
     list_of_file_names.each{|filename|
     Song.new_by_filename(filename)}
   end
