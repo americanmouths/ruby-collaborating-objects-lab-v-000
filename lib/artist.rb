087286b1e47cb27d70b96e6362dbf9f07f6c2352
Kayla Hansen <shiverr@gmail.com>
2017-10-21 22:50:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
 
  def self.find_or_create_by_name(name)
    if (self.artist.nil?)
-     self.name = Artist.new(name)
+     self.artist = Artist.new(name)
    else
      self.artist.name = name
    end
