53fa3dfbdc1d10d6e078f63b16cef18a567ff6d3
Kayla Hansen <shiverr@gmail.com>
2017-10-21 22:23:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,6 +15,12 @@ class Artist
    songs << @songs 
  end
 
+ def self.find_or_create_by_name(name)
+   if (self.artist.nil?)
+     self.artist = Artist.new(name)
+   else
+     self.artist.name = name
+
  def save
    @@all.save
  end
