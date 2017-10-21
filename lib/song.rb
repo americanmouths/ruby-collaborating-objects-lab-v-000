65f4b1a13dd981a6184f671399e7a902e4729a2f
Kayla Hansen <shiverr@gmail.com>
2017-10-21 20:49:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 class Song
-  attr_accessor :artist, :name, :title
+  attr_accessor :artist, :name, :title, :artist_name
 
   def initialize(name)
     @name = name
   end
 
   def self.new_by_filename(file_name)
+    new_instance = file_name.split(" - ")[1]
     new_instance = Song.new(file_name)
     new_instance.title = file_name.split(" - ")[1]
   end
