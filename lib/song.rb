e13268120050dad678ecbc124641fab4be0c2125
Kayla Hansen <shiverr@gmail.com>
2017-10-21 22:05:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,7 @@ class Song
 
   def self.new_by_filename(file_name)
     new_instance = file_name.split(" - ")[1]
-    new_instance.each{|filename|
-    song = Song.new(filename)}
+    new_instance = Song.new(file_name)
   end
 
 end
